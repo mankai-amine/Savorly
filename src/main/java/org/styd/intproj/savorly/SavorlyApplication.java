b17e213c3825c27c248abe8bd23c7f9bde10d205
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2025-01-10 13:22:42 -0500

    Initial Spring Boot setup

---
+++ src/main/java/org/styd/intproj/savorly/SavorlyApplication.java
@@ -0,0 +1,13 @@
+package org.styd.intproj.savorly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavorlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavorlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/styd/intproj/savorly/SavorlyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.styd.intproj.savorly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavorlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
