e8be3c85e5b51d159d5ae59c8c9f8918a2f04d8a
SDarbyson <Sean.Darbyson@johnabbottcollege.net>
2025-01-21 12:45:06 -0500

    commenting out mail

--- backend/src/main/java/org/styd/intproj/savorly/dto/MailRequest.java
+++ backend/src/main/java/org/styd/intproj/savorly/dto/MailRequest.java
@@ -1,18 +1,18 @@
-package org.styd.intproj.savorly.dto;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class MailRequest {
-    private String to;
-    private String subject;
-    private String message;
-    private String name;
-    private boolean isHtml;
-
-}
+//package org.styd.intproj.savorly.dto;
+//
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//public class MailRequest {
+//    private String to;
+//    private String subject;
+//    private String message;
+//    private String name;
+//    private boolean isHtml;
+//
+//}
